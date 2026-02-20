b58d588d7b141ec8b13fb199a63ef4a42c4a4cd2
Nawidullah Amin <nawidullah.amin1992@gmail.com>
2026-02-20 20:10:48 +0430

    Project Finalized as for now

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// Select Elements
 const userInput = document.getElementById("num");
 const btn = document.getElementById("btn");
 const result = document.querySelector(".result");
 
+// Create the runMultiplier Function
 const runMultiplier = (userInput) => {
     result.innerHTML = "";
-
+    // Use for loop to multiply a number from 1 to 10 by the user's input number
     for(let i = 1; i <= 10; i++){
         const col = document.createElement("div");
         col.classList.add("col");
@@ -15,12 +17,13 @@ const runMultiplier = (userInput) => {
             <div class="equal">&equals;</div>
             <div class="output">${i * userInput}</div>
         `;
-
+        // Add smooth animation to each row
         col.style.animationDelay = `${i * 0.2}s`;
         result.appendChild(col);
     }
 }
 
+// Add an event listener to the btn Run
 btn.addEventListener("click", (e) => {
     e.preventDefault();
     const inputValue = Number(userInput.value);
